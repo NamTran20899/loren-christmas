addec9faaed1b83301861d9618d4dad78bb0acb4
NamTran <phuongnamtran20899@gmail.com>
2024-01-24 09:00:18 +0700

    no message

---
+++ main.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function(){
+    $('.carousel').flickity({
+        // options
+        imagesLoaded: true,
+        contain: true,
+        groupCells: 2,
+        freeScroll: true,
+        wrapAround: true,
+        imagesLoaded: true,
+        pageDots: false,
+        autoPlay: 2000,
+    })
+})
+
+function popup1(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function popup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function popup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
